b6b27cdb2f9a51a146a7ca5ac4a70c080b5f0d44
Tianjun Xu <tianjun.xu@veeva.com>
2023-05-12 16:26:14 -0700

    use access token as password on source tree. remove access token on repo url. try to commit

--- src/main/java/org/springframework/samples/petclinic/PetClinicApplication.java
+++ src/main/java/org/springframework/samples/petclinic/PetClinicApplication.java
@@ -29,7 +29,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class PetClinicApplication {
 
 	public static void main(String[] args) {
-		System.out.println("hello world123");
+		System.out.println("hello world123ddd");
 		SpringApplication.run(PetClinicApplication.class, args);
 	}
 
